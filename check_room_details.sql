314e5cf4a5cddd3c732fc786b652f4bf14a79af6
Sai <202051173@iiitvadodara.ac.in>
2026-02-08 00:10:16 +0530

    Remove documentation and test files from Git, update .gitignore

--- check_room_details.sql
+++
@@ -1,7 +0,0 @@
--- Check ALL fields in the room_types table for SBH2
-SELECT rt.id, rt.name, rt.description, rt.price_per_night, 
-       rt.capacity, rt.total_rooms, rt.amenities, rt.images,
-       h.name as hotel_name
-FROM room_types rt
-JOIN hotels h ON rt.hotel_id = h.id
-WHERE h.id = 3;
